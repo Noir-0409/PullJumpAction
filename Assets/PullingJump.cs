45d2e87d68bb7c98885e84236da406709bb2e0d2
Noir-0409 <146914778+Noir-0409@users.noreply.github.com>
2024-06-10 11:52:49 +0900

    07_02完了

--- Assets/PullingJump.cs
+++ Assets/PullingJump.cs
@@ -18,8 +18,33 @@ public class PullingJump : MonoBehaviour
 
     private void OnCollisionStay(Collision collision)
     {
+        // 衝突している点の情報が複数格納されている
+        ContactPoint[] contacts = collision.contacts;
+
+        // 0番目の衝突情報から、衝突している点の法線を取得
+        Vector3 otherNormal = contacts[0].normal;
+
+        // 上方向を示すベクトル、長さは1
+        Vector3 upVector = new Vector3(0, 1, 0);
+
+        // 上方向と法線の内積。2つのベクトルはともに長さが1なので、cosΘの結果がdotUN変数に入る
+        float dotUN = Vector3.Dot(upVector, otherNormal);
+
+        // 内積値に逆三角形arccosをかけて角度を算出。それを度数法へ変換する
+        float dotDeg = Mathf.Acos(dotUN) * Mathf.Rad2Deg;
+
+        // 2つのベクトルがなす角度が45度より小さければジャンプ化の素とする
+        if (dotDeg <= 45)
+        {
+
+            isCanJump = true;
+
+        }
+
         //Debug.Log("接触中");
+
         isCanJump = true;
+
     }
 
     private void OnCollisionExit(Collision collision)
